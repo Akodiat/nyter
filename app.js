3fc46daa204dc6856df3be7027c326cf3858ce84
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2021-09-22 22:57:49 +0100

    Add Konva graphics

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ class Application {
     this.update({name: 'A', frequency: this.a4, octave: 4, value: 69, cents: 0});
     this.trackIdx = 0;
     this.noteIdx = 0;
+    this.noteMax = 128;
+
+    // Setup canvas
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    this.stage = new Konva.Stage({
+      container: 'container',
+      width: width,
+      height: height,
+    });
+
+    let layer = new Konva.Layer();
+    this.marker = new Konva.Rect({
+      x: 0,
+      y: 64,
+      width: 100,
+      height: 10,
+      fill: 'red',
+      stroke: 'black',
+      strokeWidth: 4,
+    });
+    layer.add(this.marker);
+    this.stage.add(layer);
   }
 
   initA4() {
@@ -23,6 +47,7 @@ class Application {
       if (this.notes.isAutoMode) {
         if (this.lastNote === note.name) {
           this.update(note);
+          this.updateMarker(note);
           const correct = this.tracks[this.trackIdx].notes[this.noteIdx].name;
           const current = note.name + note.octave;
           if (current === correct) {
@@ -54,6 +79,33 @@ class Application {
     this.tracks = midi.tracks;
 
     console.log("Tracks: "+this.tracks.map((t,i)=>`${i}: ${t.instrument.name}`));
+
+    this.drawTrack();
+  }
+
+  updateMarker(note) {
+    this.marker.y(this.stage.attrs.height * (note.value / this.noteMax));
+  }
+
+  drawTrack() {
+    this.trackLayer = new Konva.Layer();
+
+    for (const note of this.tracks[this.trackIdx].notes) {
+      var rect = new Konva.Rect({
+        x: note.ticks,
+        y: this.stage.attrs.height * (note.midi / this.noteMax),
+        width: note.durationTicks,
+        height: 20,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4,
+      });
+      // add the shape to the layer
+      this.trackLayer.add(rect);
+    }
+
+    // add the layer to the stage
+    this.stage.add(this.trackLayer);
   }
 
   updateFrequencyBars() {
